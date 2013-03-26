27ff455c7f2146a52fa6f5fc9068d5657ad323b1
Joe Karayusuf <jkarayusuf@gmail.com>
2013-03-25 23:01:48 -0400

    Setup failing scenario

---
+++ features/step_definitions/executable_steps.rb
@@ -0,0 +1,13 @@
+Given(/^I run the command "(.*?)"$/) do |command|
+  system("#{command} &> /dev/null")
+  @process = $?
+end
+
+When(/^I visit the url "(.*?)"$/) do |url|
+  @session = Capybara::Session.new(:selenium)
+  @session.visit url
+end
+
+Then(/^I should see the root page$/) do
+  @session.should have_content "Hello World"
+end
